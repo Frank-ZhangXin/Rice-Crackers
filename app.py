31e95910aad69a209f1e76534afbbe2460fedb9b
Frank Zhang <zxuuzx@gmail.com>
2017-01-19 16:16:05 -0600

    update app functions and upload new html template

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app=Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, world!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def greeting(name=None):
+    return render_template('hello.html', user=name)
+
+@app.route('/photo')
+def photo_index():
+    return 'Here is photo index page!'
+
+@app.route('/photo/<int:photo_id>')
+def show_photo(photo_id):
+    return 'Photo showing is No.%d' % post_id
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'Now do login() and loging in'
+    else:
+        return 'Show the log page'
 
 if __name__ == "__main__":
     app.run(debug=True)
